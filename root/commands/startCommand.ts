1e60cfae4b6941fceb42d079f50a2bfa202958df
EliotCodes <eliotcodes@gmail.com>
2024-11-17 11:37:51 +0100

    feat: add welcome message

--- root/commands/startCommand.ts
+++ root/commands/startCommand.ts
@@ -1,4 +1,11 @@
-export default function start(ctx: any) :any {
-    
+import { InlineKeyboard } from "grammy";
 
+export default async function start(ctx: any): Promise<void> {
+  const buttons = new InlineKeyboard().text("✅ Get Started", "get_started");
+  ctx.reply(
+    `Welcome ${ctx.from.first_name}, you can get free subscribers for your telegram channel by subscribing to other channels.\n\nIt's a sub-for-sub concept, click on /promote to boost your channel.`,
+    { reply_markup: buttons }
+  );
 }
+
+
