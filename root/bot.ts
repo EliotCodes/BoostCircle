252857e68715f1487638d96bf0add3a3569ab74c
EliotCodes <eliotcodes@gmail.com>
2024-11-17 11:06:44 +0100

    feat: add the main bot file

--- root/bot.ts
+++ root/bot.ts
@@ -1,7 +1,7 @@
-import 'dotenv/config';
 import { Bot } from 'grammy';
+import { config } from './config';
 
-const bot = new Bot(process.env.BOT_TOKEN as string);
+const bot = new Bot(config.bot_token);
 
 bot.command("start", (ctx) => {
     ctx.reply("Hello World");
