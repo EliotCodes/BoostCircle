089143fbf61e09185c68a8b2f110413f17882041
EliotCodes <eliotcodes@gmail.com>
2024-11-17 11:07:25 +0100

    feat: add configuration and dotenv

---
+++ root/config.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({path: path.resolve(__dirname, "..", ".env")});
+
+if (!process.env.BOT_TOKEN) throw new Error("Bot token is not set in the enviroment variable");
+
+export const config = {
+    bot_token: 
+    process.env.BOT_TOKEN,
+} 
