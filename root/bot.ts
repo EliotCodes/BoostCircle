83a6454632c914e866db5c7a8b0594fe273b6fe1
EliotCodes <eliotcodes@gmail.com>
2024-11-17 11:15:00 +0100

    feat: add startCommand

--- root/bot.ts
+++ root/bot.ts
@@ -1,11 +1,10 @@
 import { Bot } from 'grammy';
 import { config } from './config';
+import startCommand from './commands/startCommand';
 
 const bot = new Bot(config.bot_token);
 
-bot.command("start", (ctx) => {
-    ctx.reply("Hello World");
-})
+bot.command("start", ctx => startCommand(ctx))
 
 bot.start();
 
---
+++ root/commands/startCommand.ts
@@ -0,0 +1,4 @@
+export default function start(ctx: any) :any {
+    
+
+}
