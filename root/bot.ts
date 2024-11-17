6c935aa60becb50690f7e72b8fcbef6e73b6b79d
EliotCodes <eliotcodes@gmail.com>
2024-11-17 23:12:20 +0100

    feat: add getStarted callback query

--- root/bot.ts
+++ root/bot.ts
@@ -1,10 +1,13 @@
 import { Bot } from 'grammy';
 import { config } from './config';
+
 import startCommand from './commands/startCommand';
+import getStarted from './callbacks/getStartedCallback';
 
 const bot = new Bot(config.bot_token);
 
-bot.command("start", ctx => startCommand(ctx))
+bot.command("start", ctx => startCommand(ctx));
+bot.callbackQuery("get_started", ctx => getStarted(ctx));
 
 bot.start();
 
---
+++ root/callbacks/getStartedCallback.ts
@@ -0,0 +1,7 @@
+import { InlineKeyboard } from "grammy"
+
+export default async function getStarted(ctx:any) {
+    // const inlineButtons = new InlineKeyboard()
+    // .text("")
+    // ctx.editMessageText("Hello")
+}
