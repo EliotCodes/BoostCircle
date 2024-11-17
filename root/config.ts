003e25e74df26800eb6f56924c91388d3726c6b8
EliotCodes <eliotcodes@gmail.com>
2024-11-17 23:13:09 +0100

    refactor: format codes to improve readability

--- root/config.ts
+++ root/config.ts
@@ -1,11 +1,10 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
-dotenv.config({path: path.resolve(__dirname, "..", ".env")});
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 if (!process.env.BOT_TOKEN) throw new Error("Bot token is not set in the enviroment variable");
 
 export const config = {
-    bot_token: 
-    process.env.BOT_TOKEN,
+    bot_token: process.env.BOT_TOKEN,
 } 
